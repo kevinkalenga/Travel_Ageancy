068decde53d155f213096a37e9222f91078612b3
Kalenga243 <kevinkalenga10@gmail.com>
2025-08-11 23:59:33 +0200

    Login and Dashboard

--- Travel_Ageancy/app/Http/Controllers/Admin/AdminAuthController.php
+++ Travel_Ageancy/app/Http/Controllers/Admin/AdminAuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Auth;
+use Hash;
+use App\Models\Admin;
 
 class AdminAuthController extends Controller
 {
@@ -11,6 +14,34 @@ class AdminAuthController extends Controller
     {
         return view('admin.login');
     }
+    public function login_submit(Request $request) 
+    {
+        $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+       ]);
+      //check contain email ans pwd of the request   
+       $check = $request->all();
+       $data = [
+        'email' => $check['email'],
+        'password' => $check['password']
+       ];
+        // it's calling the admin model if everything is ok
+        if(Auth::guard('admin')->attempt($data)) {
+            return redirect()->route('admin_dashboard')->with('success', 'Login is successful!');
+        } else {
+          return redirect()->route('admin_login')->with('error','The information you entered is incorrect! Please try again!');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin_login')->with('success','Logout is successful!');
+    }
+    
+    
+    
     public function profile() 
     {
         return view('admin.profile');
--- Travel_Ageancy/routes/web.php
+++ Travel_Ageancy/routes/web.php
@@ -8,12 +8,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// You must login first before you access
+Route::middleware('admin')->prefix('admin')->group(function() {
+    Route::get('/profile',[AdminAuthController::class,'profile'])->name('admin_profile');
+    Route::get('/dashboard',[AdminDashboardController::class,'dashboard'])->name('admin_dashboard');
+});
+
 // Admin
 Route::prefix('admin')->group(function() {
     Route::get('/login',[AdminAuthController::class,'login'])->name('admin_login');
+    Route::get('/logout',[AdminAuthController::class,'logout'])->name('admin_logout');
+    Route::post('/login',[AdminAuthController::class,'login_submit'])->name('admin_login_submit');
     Route::get('/forget-password',[AdminAuthController::class,'forget_password'])->name('admin_forget_password');
     Route::get('/reset-password',[AdminAuthController::class,'reset_password'])->name('admin_reset_password');
-    Route::get('/profile',[AdminAuthController::class,'profile'])->name('admin_profile');
-    Route::get('/dashboard',[AdminDashboardController::class,'dashboard'])->name('admin_dashboard');
 
+    
 });
