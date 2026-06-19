6b1be4cb96a4d016f106f797eedac157bb568471
kevin <kevinkalenga10@gmail.com>
2026-06-19 15:32:44 +0200

    TrustProxies and AppServiceProvider modified

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-        if (env('APP_ENV') === 'production') {
-          URL::forceScheme('https');
-        }
+        // if (env('APP_ENV') === 'production') {
+        //   URL::forceScheme('https');
+        // }
     }
 }
